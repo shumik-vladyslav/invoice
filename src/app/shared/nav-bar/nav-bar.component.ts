27a5612e2f16ecc2136ad7a10ce5ed1fbd9419d0
Vladyslav Shumik <shumik.vladyslav@gmail.com>
2018-06-30 17:39:24 +0300

    create content page

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.sass']
 })
 export class NavBarComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit() {
