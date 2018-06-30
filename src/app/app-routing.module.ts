030381744d94ac6151939c8a5e0b192fad04d98a
shumik vlad <shumik.vladyslav@gmail.com@>
2018-06-30 13:59:50 +0300

    create arhitecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InvoiceComponent} from './pages/invoice/invoice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component: InvoiceComponent, path: "invoice"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
